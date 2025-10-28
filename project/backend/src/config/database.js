b5103e25fc6832743c4e3fcce8d8608760d926c7
Akshat <121187184+AkshatShrivastava0104@users.noreply.github.com>
2025-10-29 05:14:37 +0530

    updated Config file in database.js

--- project/backend/src/config/database.js
+++ project/backend/src/config/database.js
@@ -3,31 +3,39 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'uber_rescue',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'uberrescue',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: console.log
   },
+
   test: {
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME + '_test' || 'uber_rescue_test',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME + '_test' || 'uberrescue_test',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false
   },
+
   production: {
-    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'uberrescue',
+    host: process.env.DB_HOST || 'postgres',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
+    logging: false,
+    dialectOptions: process.env.DB_SSL === 'true'
+      ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
       }
-    },
-    logging: false
+      : {}
   }
 };
